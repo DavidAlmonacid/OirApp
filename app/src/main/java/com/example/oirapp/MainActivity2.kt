dc67981c14ac61ca30001c8920b4f72215313ad3
David Almonacid <david.almonacidf@gmail.com>
2024-07-12 21:34:19 -0500

    feat: Add click listener to textView4 in MainActivity2

--- app/src/main/java/com/example/oirapp/MainActivity2.kt
+++ app/src/main/java/com/example/oirapp/MainActivity2.kt
@@ -1,6 +1,8 @@
 package com.example.oirapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity2 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val textView: TextView = findViewById(R.id.textView4)
+        textView.setOnClickListener {
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
         }
     }
 }
