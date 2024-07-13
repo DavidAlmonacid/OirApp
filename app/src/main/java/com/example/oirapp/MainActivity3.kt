fb662444d34b297b364f24fdc89c1a50a946d79d
David Almonacid <david.almonacidf@gmail.com>
2024-07-13 13:02:23 -0500

    Update MainActivity3 with spinner for selecting roles

--- app/src/main/java/com/example/oirapp/MainActivity3.kt
+++ app/src/main/java/com/example/oirapp/MainActivity3.kt
@@ -1,6 +1,8 @@
 package com.example.oirapp
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,17 @@ class MainActivity3 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.roles_array,
+            R.layout.spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+            spinner.adapter = adapter
         }
     }
 }
