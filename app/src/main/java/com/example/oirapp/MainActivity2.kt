756d46f1b2d68c0a4d08d877fc4b6edfd9b45e2d
Francisco <franciscogarzonmoreno@gmail.com>
2024-07-17 21:56:23 -0500

    se programaron edittext de correo y contraseña y y se autenticaron

--- app/src/main/java/com/example/oirapp/MainActivity2.kt
+++ app/src/main/java/com/example/oirapp/MainActivity2.kt
@@ -2,13 +2,20 @@ package com.example.oirapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,11 +25,33 @@ class MainActivity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        auth = Firebase.auth
+        val button: Button = findViewById(R.id.button)
+        val email:EditText = findViewById(R.id.editTextTextEmailAddress)
+        val password:EditText = findViewById(R.id.editTextTextPassword)
+        button.setOnClickListener {
+            signIn(email.text.toString(), password.text.toString())}
         val textView: TextView = findViewById(R.id.textView4)
         textView.setOnClickListener {
             val intent = Intent(this, MainActivity3::class.java)
             startActivity(intent)
         }
+    }
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val intent = Intent(this, MainActivity5::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
     }
 }
