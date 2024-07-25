a1436ec058685b7f8118fddda4de85c1aafea215
David Almonacid <david.almonacidf@gmail.com>
2024-07-25 11:52:26 -0500

    chore: Update MainActivity5 layout and add Spinner for role selection

--- app/src/main/java/com/example/oirapp/MainActivity5.kt
+++ app/src/main/java/com/example/oirapp/MainActivity5.kt
@@ -1,8 +1,9 @@
 package com.example.oirapp
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ImageView
-import android.widget.LinearLayout
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,8 +11,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
 
 class MainActivity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,25 +23,28 @@ class MainActivity5 : AppCompatActivity() {
             insets
         }
 
-        // TODO: Get the user email from Firebase Auth and display it in the TextView
         // TODO: Implement the functionality to sign in the user automatically if they are already signed in
-        // TODO: Implement the functionality to sign out the user
 
         val imageView: ImageView = findViewById(R.id.user_profile_image)
-        Glide.with(this)
-            .load(R.drawable.user_placeholder)
-            .transform(CenterCrop(), RoundedCornersTransformation(32))
-            .into(imageView)
-        // Recuperar el correo electrónico del Intent
-        val userEmail = intent.getStringExtra("USER_EMAIL")
+        Glide.with(this).load(R.drawable.user_placeholder)
+            .transform(CenterCrop(), RoundedCornersTransformation(32)).into(imageView)
 
-        // Hacer algo con el correo electrónico, por ejemplo, mostrarlo en un TextView
-        val emailTextView: TextView = findViewById(R.id.textView11)
+        val userEmail = intent.getStringExtra("USER_EMAIL")
+        val emailTextView: TextView = findViewById(R.id.textView2)
         emailTextView.text = userEmail
-        val signOutButton: LinearLayout = findViewById(R.id.Cerar_sesion)
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+        ArrayAdapter.createFromResource(
+            this, R.array.roles_array, R.layout.spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        /*val signOutButton: LinearLayout = findViewById(R.id.Cerar_sesion)
         signOutButton.setOnClickListener {
             Firebase.auth.signOut()
             finish()
-        }
+        }*/
     }
 }
