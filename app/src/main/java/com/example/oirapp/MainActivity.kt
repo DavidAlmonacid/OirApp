f8c29b61a7600bb2c210fab5f04eeb1f7cfd67a5
David Almonacid <david.almonacidf@gmail.com>
2024-07-30 16:35:49 -0500

    feat: Add Firebase authentication to check if the user is already logged in.

--- app/src/main/java/com/example/oirapp/MainActivity.kt
+++ app/src/main/java/com/example/oirapp/MainActivity.kt
@@ -7,8 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,10 +24,24 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        auth = Firebase.auth
+
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this, MainActivity5::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
