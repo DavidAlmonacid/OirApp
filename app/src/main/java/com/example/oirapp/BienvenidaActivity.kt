cd7c347bc7fc97e1ea34de45a437b93f4213c435
David Almonacid <david.almonacidf@gmail.com>
2024-08-16 16:40:15 -0500

    Rename files

--- app/src/main/java/com/example/oirapp/BienvenidaActivity.kt
+++ app/src/main/java/com/example/oirapp/BienvenidaActivity.kt
@@ -2,23 +2,27 @@ package com.example.oirapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.oirapp.databinding.ActivityBienvenidaBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 
-class MainActivity : AppCompatActivity() {
+class BienvenidaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBienvenidaBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityBienvenidaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bienvenida)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -26,8 +30,7 @@ class MainActivity : AppCompatActivity() {
 
         auth = Firebase.auth
 
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
+        binding.button.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
             finish()
