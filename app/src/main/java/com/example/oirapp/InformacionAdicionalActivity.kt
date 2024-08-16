e5ee3b1d952c7d1023885f333420755e9be24be3
David Almonacid <david.almonacidf@gmail.com>
2024-08-16 16:43:04 -0500

    Prueba del botón de cerrar sesión

--- app/src/main/java/com/example/oirapp/InformacionAdicionalActivity.kt
+++ app/src/main/java/com/example/oirapp/InformacionAdicionalActivity.kt
@@ -1,6 +1,7 @@
 package com.example.oirapp
 
 import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -14,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.oirapp.databinding.ActivityInformacionAdicionalBinding
 import com.github.dhaval2404.imagepicker.ImagePicker
 import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.google.firebase.database.database
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
@@ -77,8 +79,7 @@ class InformacionAdicionalActivity : AppCompatActivity() {
             myRef.setValue(usuario)
         }
 
-        /*val signOutButton: Button = findViewById(R.id.button2)
-        signOutButton.setOnClickListener {
+        /*binding.signOutButton.setOnClickListener {
             Firebase.auth.signOut()
             finish()
 
