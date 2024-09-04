08b000870fa191f91f327e87c2c709c2f5d1078d
Francisco <franciscogarzonmoreno@gmail.com>
2024-09-03 21:50:11 -0500

    correcion error en archivo mensaje dao

--- app/src/main/java/com/example/oirapp/data/database/MensajeDao.kt
+++ app/src/main/java/com/example/oirapp/data/database/MensajeDao.kt
@@ -11,9 +11,9 @@ interface MensajeDao {
     @Insert
     suspend fun insertMensaje(mensaje: Mensaje)
 
-    @Query("SELECT * FROM Mensaje WHERE id_grupo = :idGrupo ORDER BY fecha_envio ASC")
+    @Query("SELECT * FROM mensajes WHERE id_grupo = :idGrupo ORDER BY fecha_envio ASC")
     suspend fun getMensajesByGrupo(idGrupo: Int): List<Mensaje>
 
-    @Query("DELETE FROM Mensaje WHERE id_mensaje = :id")
+    @Query("DELETE FROM mensajes WHERE id_mensaje = :id")
     suspend fun deleteMensaje(id: Int)
 }
