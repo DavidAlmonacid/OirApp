5a7c10e9a78e2d259b1a3efbca3c4eb34b68263e
David Almonacid <david.almonacidf@gmail.com>
2024-09-06 08:00:01 -0500

    Utilización de las variables de entorno

--- app/src/main/java/com/example/oirapp/data/network/SupabaseClient.kt
+++ app/src/main/java/com/example/oirapp/data/network/SupabaseClient.kt
@@ -2,11 +2,12 @@ package com.example.oirapp.data.network
 
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
+import com.example.oirapp.BuildConfig
 
 object SupabaseClient {
     val supabaseClient = createSupabaseClient(
-        supabaseUrl = "https://lydqdbpjgddgkzcdooqe.supabase.co",
-        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5ZHFkYnBqZ2RkZ2t6Y2Rvb3FlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU1NTE3NDIsImV4cCI6MjA0MTEyNzc0Mn0.SrJ_Ln3HB6o_EXdKp26raJrdhh8W6WdDuW6UZHJ8PUg",
+        supabaseUrl = BuildConfig.SUPABASE_URL,
+        supabaseKey = BuildConfig.SUPABASE_KEY,
     ) {
         install(Postgrest)
     }
