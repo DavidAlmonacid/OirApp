efa6d448b27d0b3384d592672cc528eae0282620
David Almonacid <david.almonacidf@gmail.com>
2024-09-07 16:52:26 -0500

    Instalado servicio de autenticación de supabase

--- app/src/main/java/com/example/oirapp/data/network/SupabaseClient.kt
+++ app/src/main/java/com/example/oirapp/data/network/SupabaseClient.kt
@@ -3,12 +3,14 @@ package com.example.oirapp.data.network
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import com.example.oirapp.BuildConfig
+import io.github.jan.supabase.gotrue.Auth
 
 object SupabaseClient {
     val supabaseClient = createSupabaseClient(
         supabaseUrl = BuildConfig.SUPABASE_URL,
         supabaseKey = BuildConfig.SUPABASE_KEY,
     ) {
+        install(Auth)
         install(Postgrest)
     }
 }
