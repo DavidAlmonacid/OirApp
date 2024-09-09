4ebd02048fb7d47aa92ef086dd4b5a9060bddf8a
David Almonacid <david.almonacidf@gmail.com>
2024-09-09 02:09:01 -0500

    Mejoarada la UI para la presentación de los datos del usuario con su respectivo rol

--- app/src/main/java/com/example/oirapp/docente/GruposDocenteActivity.kt
+++ app/src/main/java/com/example/oirapp/docente/GruposDocenteActivity.kt
@@ -5,17 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.oirapp.R
+import com.example.oirapp.databinding.ActivityGruposDocenteBinding
 
 class GruposDocenteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGruposDocenteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_grupos_docente)
+
+        binding = ActivityGruposDocenteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Get the data from the previous activity
+        val userImageUrl = intent.getStringExtra("USER_IMAGEN_URL")!!
+        val userName = intent.getStringExtra("USER_NOMBRE")!!
+        val userRole = intent.getStringExtra("USER_ROL")!!
+
+        // Set the user's name and role
+        binding.docenteNombreTv.text = userName
+        binding.docenteRolTv.text = userRole
+
+        // Load the user's image
+        Glide.with(this)
+            .load(userImageUrl)
+            .placeholder(R.drawable.user_placeholder)
+            .error(R.drawable.user_placeholder)
+            .into(binding.docenteImagenIv)
     }
 }
--- app/src/main/java/com/example/oirapp/estudiante/GruposEstudianteActivity.kt
+++ app/src/main/java/com/example/oirapp/estudiante/GruposEstudianteActivity.kt
@@ -5,17 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.oirapp.R
+import com.example.oirapp.databinding.ActivityGruposEstudianteBinding
 
 class GruposEstudianteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGruposEstudianteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_grupos_estudiante)
+
+        binding = ActivityGruposEstudianteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Get the data from the previous activity
+        val userImageUrl = intent.getStringExtra("USER_IMAGEN_URL")!!
+        val userName = intent.getStringExtra("USER_NOMBRE")!!
+        val userRole = intent.getStringExtra("USER_ROL")!!
+
+        // Set the user's name and role
+        binding.estudianteNombreTv.text = userName
+        binding.estudianteRolTv.text = userRole
+
+        // Load the user's image
+        Glide.with(this)
+            .load(userImageUrl)
+            .placeholder(R.drawable.user_placeholder)
+            .error(R.drawable.user_placeholder)
+            .into(binding.estudianteImagenIv)
     }
 }
