179c18e4e69fa48b9ed3037cc9a424a39276cef4
David Almonacid <david.almonacidf@gmail.com>
2024-09-12 16:33:37 -0500

    Refactor BienvenidaScreen.kt for custom text styling and layout improvements

--- app/src/main/java/com/example/oirapp/ui/BienvenidaScreen.kt
+++ app/src/main/java/com/example/oirapp/ui/BienvenidaScreen.kt
@@ -1,37 +1,42 @@
 package com.example.oirapp.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.oirapp.R
-import com.example.oirapp.ui.theme.MyApplicationTheme
+import com.example.oirapp.ui.components.CustomFamilyText
 
 @Composable
 fun BienvenidaScreen(modifier: Modifier = Modifier) {
     Surface(
-        color = MaterialTheme.colorScheme.primaryContainer,
+        color = MaterialTheme.colorScheme.primary,
         modifier = modifier.fillMaxSize(),
     ) {
-        Box(modifier = Modifier.padding(top = 16.dp)) {
+        Box(modifier = Modifier.padding(top = 28.dp)) {
             Card(
                 colors = CardColors(
                     containerColor = MaterialTheme.colorScheme.surface,
@@ -39,24 +44,19 @@ fun BienvenidaScreen(modifier: Modifier = Modifier) {
                     disabledContentColor = MaterialTheme.colorScheme.onSurface,
                     disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
                 ),
+                shape = MaterialTheme.shapes.large,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 24.dp)
-                    .padding(top = 290.dp),
+                    .padding(top = 280.dp),
             ) {
-                Text(
-                    text = stringResource(R.string.bienvenida1),
-                    modifier = Modifier
-                        .padding(horizontal = 24.dp)
-                        .padding(top = 32.dp),
-                )
-
-                Text(
-                    text = stringResource(R.string.bienvenida2),
-                    modifier = Modifier
-                        .padding(horizontal = 24.dp)
-                        .padding(top = 20.dp, bottom = 32.dp),
-                )
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(20.dp),
+                    modifier = Modifier.padding(horizontal = 24.dp, vertical = 32.dp),
+                ) {
+                    CustomFamilyText(text = stringResource(R.string.bienvenida1))
+                    CustomFamilyText(text = stringResource(R.string.bienvenida2))
+                }
             }
 
             Image(
@@ -67,7 +67,7 @@ fun BienvenidaScreen(modifier: Modifier = Modifier) {
                     .align(Alignment.TopCenter),
             )
 
-            Text(
+            CustomFamilyText(
                 text = stringResource(R.string.app_name),
                 style = MaterialTheme.typography.displayMedium,
                 textAlign = TextAlign.End,
@@ -75,27 +75,29 @@ fun BienvenidaScreen(modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .align(Alignment.TopEnd)
                     .widthIn(max = 120.dp)
-                    .padding(top = 20.dp, end = 24.dp),
+                    .padding(top = 8.dp, end = 24.dp),
             )
 
             OutlinedButton(
                 onClick = { /*TODO*/ },
+                colors = ButtonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = MaterialTheme.colorScheme.onPrimary,
+                    disabledContentColor = MaterialTheme.colorScheme.onSurface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                ),
+                border = BorderStroke(1.dp, MaterialTheme.colorScheme.onPrimary),
+                shape = MaterialTheme.shapes.medium,
+                contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp),
                 modifier = Modifier
                     .align(Alignment.BottomEnd)
                     .padding(end = 24.dp, bottom = 28.dp),
             ) {
-                Text(
-                    text = "Inicio"
+                CustomFamilyText(
+                    text = stringResource(R.string.inicio),
+                    fontSize = 18.sp,
                 )
             }
         }
-    }
-}
-
-@Preview(showBackground = true, device = "id:pixel_5", apiLevel = 28)
-@Composable
-private fun BienvenidaScreenPreview() {
-    MyApplicationTheme {
-        BienvenidaScreen()
     }
 }
