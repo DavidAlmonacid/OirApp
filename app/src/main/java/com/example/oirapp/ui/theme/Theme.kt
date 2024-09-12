8ab4aa720e2188e6c21f539d0154b77fd95b920e
David Almonacid <david.almonacidf@gmail.com>
2024-09-12 16:32:44 -0500

    Update color and theme files

--- app/src/main/java/com/example/oirapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/oirapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.oirapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -14,13 +13,14 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = PurpleEminence,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    surface = GhostWhite,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
