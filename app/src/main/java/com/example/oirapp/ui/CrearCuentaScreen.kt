b6d3b4a2b11c13709743102fd4a576e3fa20e8a4
David Almonacid <david.almonacidf@gmail.com>
2024-09-13 15:05:51 -0500

    Add CrearCuentaScreen.kt file

---
+++ app/src/main/java/com/example/oirapp/ui/CrearCuentaScreen.kt
@@ -0,0 +1,95 @@
+package com.example.oirapp.ui
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.oirapp.R
+import com.example.oirapp.ui.components.CustomButton
+import com.example.oirapp.ui.components.CustomFamilyText
+import com.example.oirapp.ui.theme.MyApplicationTheme
+
+@Composable
+fun CrearCuentaScreen(modifier: Modifier = Modifier) {
+    val emailState = remember { mutableStateOf("") }
+    val passwordState = remember { mutableStateOf("") }
+
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = modifier.fillMaxSize(),
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(horizontal = 24.dp)
+                .padding(top = 32.dp),
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = stringResource(R.string.logo_description),
+                modifier = Modifier.size(152.dp),
+            )
+
+            OutlinedTextField(
+                value = emailState.value,
+                onValueChange = { emailState.value = it },
+                label = { CustomFamilyText(R.string.email) },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small,
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = MaterialTheme.colorScheme.onSurface,
+                ),
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+            OutlinedTextField(
+                value = passwordState.value,
+                onValueChange = { passwordState.value = it },
+                label = { CustomFamilyText(R.string.password) },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                singleLine = true,
+                shape = MaterialTheme.shapes.small,
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = MaterialTheme.colorScheme.onSurface,
+                ),
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+            CustomButton(onClick = { /*TODO*/ }, textId = R.string.crear_cuenta)
+        }
+    }
+}
+
+@Preview(device = "id:pixel_5", apiLevel = 28, showBackground = true)
+@Composable
+private fun CrearCuentaScreenPreview() {
+    MyApplicationTheme {
+        CrearCuentaScreen()
+    }
+}
