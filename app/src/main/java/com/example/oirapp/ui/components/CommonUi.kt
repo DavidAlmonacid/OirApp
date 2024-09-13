47fb5c8c8fe2b6f2192338d68220fd126549b6ef
David Almonacid <david.almonacidf@gmail.com>
2024-09-13 15:03:24 -0500

    Refactor CommonUi.kt to use string resources and add CustomButton

--- app/src/main/java/com/example/oirapp/ui/components/CommonUi.kt
+++ app/src/main/java/com/example/oirapp/ui/components/CommonUi.kt
@@ -1,10 +1,17 @@
 package com.example.oirapp.ui.components
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -15,6 +22,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.oirapp.R
 
 val WorkSansFamily = FontFamily(
@@ -27,13 +36,12 @@ val WorkSansFamily = FontFamily(
 
 @Composable
 fun CustomFamilyText(
-    text: String,
+    @StringRes textId: Int,
     modifier: Modifier = Modifier,
     color: Color = Color.Unspecified,
     fontSize: TextUnit = TextUnit.Unspecified,
     fontStyle: FontStyle? = null,
     fontWeight: FontWeight? = null,
-    fontFamily: FontFamily = WorkSansFamily,
     letterSpacing: TextUnit = TextUnit.Unspecified,
     textDecoration: TextDecoration? = null,
     textAlign: TextAlign? = null,
@@ -46,13 +54,13 @@ fun CustomFamilyText(
     style: TextStyle = LocalTextStyle.current,
 ) {
     Text(
-        text = text,
+        text = stringResource(textId),
         modifier = modifier,
         color = color,
         fontSize = fontSize,
         fontStyle = fontStyle,
         fontWeight = fontWeight,
-        fontFamily = fontFamily,
+        fontFamily = WorkSansFamily,
         letterSpacing = letterSpacing,
         textDecoration = textDecoration,
         textAlign = textAlign,
@@ -64,4 +72,28 @@ fun CustomFamilyText(
         onTextLayout = onTextLayout,
         style = style,
     )
+}
+
+@Composable
+fun CustomButton(
+    onClick: () -> Unit,
+    @StringRes textId: Int,
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.secondary,
+            contentColor = MaterialTheme.colorScheme.onSecondary,
+        ),
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.onSecondary),
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        CustomFamilyText(
+            textId = textId,
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Medium,
+            fontSize = 20.sp,
+        )
+    }
 }
