bb89c46fea1c375b6584ab03278e2ec2eabc36c7
David Almonacid <david.almonacidf@gmail.com>
2024-09-12 19:09:32 -0500

    Refactor BienvenidaScreen.kt to improve colors

--- app/src/main/java/com/example/oirapp/ui/BienvenidaScreen.kt
+++ app/src/main/java/com/example/oirapp/ui/BienvenidaScreen.kt
@@ -11,24 +11,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.oirapp.R
 import com.example.oirapp.ui.components.CustomFamilyText
+import com.example.oirapp.ui.theme.MyApplicationTheme
 
 @Composable
 fun BienvenidaScreen(onStartButtonClicked: () -> Unit, modifier: Modifier = Modifier) {
@@ -38,11 +39,9 @@ fun BienvenidaScreen(onStartButtonClicked: () -> Unit, modifier: Modifier = Modi
     ) {
         Box(modifier = Modifier.padding(top = 28.dp)) {
             Card(
-                colors = CardColors(
+                colors = CardDefaults.cardColors(
                     containerColor = MaterialTheme.colorScheme.surface,
                     contentColor = MaterialTheme.colorScheme.onSurface,
-                    disabledContentColor = MaterialTheme.colorScheme.onSurface,
-                    disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
                 ),
                 shape = MaterialTheme.shapes.large,
                 modifier = Modifier
@@ -80,11 +79,8 @@ fun BienvenidaScreen(onStartButtonClicked: () -> Unit, modifier: Modifier = Modi
 
             OutlinedButton(
                 onClick = onStartButtonClicked,
-                colors = ButtonColors(
-                    containerColor = Color.Transparent,
+                colors = ButtonDefaults.outlinedButtonColors(
                     contentColor = MaterialTheme.colorScheme.onPrimary,
-                    disabledContentColor = MaterialTheme.colorScheme.onSurface,
-                    disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
                 ),
                 border = BorderStroke(1.dp, MaterialTheme.colorScheme.onPrimary),
                 shape = MaterialTheme.shapes.medium,
@@ -99,5 +95,13 @@ fun BienvenidaScreen(onStartButtonClicked: () -> Unit, modifier: Modifier = Modi
                 )
             }
         }
+    }
+}
+
+@Preview(device = "id:pixel_5", apiLevel = 28, showBackground = true)
+@Composable
+private fun IniciarSesionScreenPreview() {
+    MyApplicationTheme {
+        BienvenidaScreen(onStartButtonClicked = {})
     }
 }
