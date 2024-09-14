3d8c83b6c22fc91057b97e36568717a228e7c628
David Almonacid <david.almonacidf@gmail.com>
2024-09-14 09:56:28 -0500

    Add user state flow to handle the user inputs

--- app/src/main/java/com/example/oirapp/ui/MainViewModel.kt
+++ app/src/main/java/com/example/oirapp/ui/MainViewModel.kt
@@ -1,11 +1,31 @@
 package com.example.oirapp.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.oirapp.MainApplication
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel : ViewModel() {
+    private val _userUiState = MutableStateFlow(UserUiState())
+    val userUiState: StateFlow<UserUiState> = _userUiState.asStateFlow()
+
+    var userEmail by mutableStateOf("")
+        private set
+
+    fun updateUserEmail(email: String){
+        userEmail = email
+    }
+
+    fun checkUserEmail() {
+        TODO("Get the logic to allow a valid email")
+    }
+
     private val _currentScreen = MutableLiveData(MainApplication.Bienvenida)
     val currentScreen: LiveData<MainApplication> = _currentScreen
 
