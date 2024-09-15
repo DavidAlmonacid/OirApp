e58a40f64748ec86e4eee43f7d1f442e90c1c7df
David Almonacid <david.almonacidf@gmail.com>
2024-09-14 20:29:25 -0500

    Refactor string resource handling in GruposDocenteScreen.kt and GruposEstudianteScreen.kt

---
+++ app/src/main/java/com/example/oirapp/ui/GruposEstudianteScreen.kt
@@ -0,0 +1,22 @@
+package com.example.oirapp.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.oirapp.R
+import com.example.oirapp.ui.components.CustomFamilyText
+
+@Composable
+fun GruposEstudianteScreen(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        CustomFamilyText(text = stringResource(R.string.rol_estudiante))
+    }
+}
