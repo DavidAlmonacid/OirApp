e6d5ee47c19cda06a88315ff268241585514be5d
David Almonacid <david.almonacidf@gmail.com>
2024-09-16 17:01:52 -0500

    Refactor GruposEstudianteScreen.kt to update UI components and use Text instead of CustomFamilyText

--- app/src/main/java/com/example/oirapp/ui/GruposEstudianteScreen.kt
+++ app/src/main/java/com/example/oirapp/ui/GruposEstudianteScreen.kt
@@ -3,12 +3,12 @@ package com.example.oirapp.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.oirapp.R
-import com.example.oirapp.ui.components.CustomFamilyText
 
 @Composable
 fun GruposEstudianteScreen(modifier: Modifier = Modifier) {
@@ -17,6 +17,6 @@ fun GruposEstudianteScreen(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxSize()
     ) {
-        CustomFamilyText(text = stringResource(R.string.rol_estudiante))
+        Text(text = stringResource(R.string.rol_estudiante))
     }
 }
