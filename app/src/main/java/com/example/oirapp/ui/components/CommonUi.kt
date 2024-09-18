4e8cd733228f3b977ebf8023e1e400742f2d36f6
David Almonacid <david.almonacidf@gmail.com>
2024-09-17 21:59:31 -0500

    Add UserInfo composable

--- app/src/main/java/com/example/oirapp/ui/components/CommonUi.kt
+++ app/src/main/java/com/example/oirapp/ui/components/CommonUi.kt
@@ -2,7 +2,14 @@ package com.example.oirapp.ui.components
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -21,10 +28,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.oirapp.R
@@ -154,6 +165,51 @@ private fun SelectRoleDropdownPreview() {
             options = listOf(R.string.rol_estudiante, R.string.rol_docente),
             selectedOption = "",
             onOptionSelected = {},
+        )
+    }
+}
+
+@Composable
+fun UserInfo(
+    userName: String,
+    userRole: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(modifier = modifier.fillMaxWidth()) {
+        Image(
+            painter = painterResource(R.drawable.user_placeholder),
+            contentDescription = null,
+            modifier = Modifier
+                .size(50.dp)
+                .clip(MaterialTheme.shapes.small),
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column {
+            Text(
+                text = userName,
+                fontSize = 18.sp,
+                modifier = Modifier.padding(bottom = 4.dp),
+            )
+
+            Text(
+                text = userRole,
+                fontSize = 14.sp,
+                modifier = Modifier.alpha(0.8f),
+            )
+        }
+    }
+}
+
+@CustomPreview
+@Composable
+private fun UserInfoPreview() {
+    MyApplicationTheme {
+        UserInfo(
+            userName = "David",
+            userRole = "Estudiante",
+            modifier = Modifier.padding(16.dp),
         )
     }
 }
