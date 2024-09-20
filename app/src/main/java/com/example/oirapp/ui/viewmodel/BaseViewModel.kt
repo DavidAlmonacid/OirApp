2ab48ff347d6d053e0b9f892494b4c75153a5ba9
David Almonacid <david.almonacidf@gmail.com>
2024-09-20 15:36:12 -0500

    Refactor BaseViewModel to use a single LiveData for showing dialogs

--- app/src/main/java/com/example/oirapp/ui/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/example/oirapp/ui/viewmodel/BaseViewModel.kt
@@ -5,17 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 open class BaseViewModel : ViewModel() {
-    private val _showSuccessDialog = MutableLiveData(false)
-    val showSuccessDialog: LiveData<Boolean> = _showSuccessDialog
+    private val _showDialog = MutableLiveData(false)
+    val showDialog: LiveData<Boolean> = _showDialog
 
-    private val _showErrorDialog = MutableLiveData(false)
-    val showErrorDialog: LiveData<Boolean> = _showErrorDialog
-
-    fun setShowSuccessDialog(show: Boolean) {
-        _showSuccessDialog.value = show
-    }
-
-    fun setShowErrorDialog(show: Boolean) {
-        _showErrorDialog.value = show
+    fun setShowDialog(show: Boolean) {
+        _showDialog.value = show
     }
 }
