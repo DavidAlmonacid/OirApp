dc97489b0990392a3a42cccafa9de6bb7572ed52
David Almonacid <david.almonacidf@gmail.com>
2024-09-20 11:16:07 -0500

    Refactor MainActivity.kt to improve onStart method and add TODO for redirecting user based on role

--- app/src/main/java/com/example/oirapp/MainActivity.kt
+++ app/src/main/java/com/example/oirapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.oirapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,9 +17,18 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-//    override fun onStart() {
-//        super.onStart()
-//
+    override fun onStart() {
+        super.onStart()
+
+        println("onStart: Starting App")
+
+        /*
+         * TODO: Verificar si el usuario ya ha iniciado sesión,
+         *  si es así, redirigir a la pantalla de Grupos dependiendo del rol del usuario,
+         *  si es 'Estudiante' redirigir a la pantalla de Grupos Estudiante,
+         *  si es 'Docente' redirigir a la pantalla de Grupos Docente.
+         */
+
 //        val currentUser = auth.currentUser
 //        if (currentUser != null) {
 //            val intent = Intent(this, InformacionAdicionalActivity::class.java)
@@ -28,5 +36,5 @@ class MainActivity : ComponentActivity() {
 //            startActivity(intent)
 //            finish()
 //        }
-//    }
+    }
 }
