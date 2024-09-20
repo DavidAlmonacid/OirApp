f17a7d2a22ff5b6f951e9a217cfaeb4096c92e0f
David Almonacid <david.almonacidf@gmail.com>
2024-09-20 11:15:50 -0500

    Refactor NavigationViewModel to use SavedStateHandle for storing and retrieving the current screen

--- app/src/main/java/com/example/oirapp/ui/viewmodel/NavigationViewModel.kt
+++ app/src/main/java/com/example/oirapp/ui/viewmodel/NavigationViewModel.kt
@@ -1,15 +1,20 @@
 package com.example.oirapp.ui.viewmodel
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.oirapp.MainApplication
 
-class NavigationViewModel : ViewModel() {
-    private val _currentScreen = MutableLiveData(MainApplication.Bienvenida)
+class NavigationViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+    companion object {
+        private const val CURRENT_SCREEN_KEY = "current_screen"
+    }
+
+    private val _currentScreen = savedStateHandle.getLiveData(CURRENT_SCREEN_KEY, MainApplication.Bienvenida)
     val currentScreen: LiveData<MainApplication> = _currentScreen
 
     fun updateCurrentScreen(screen: MainApplication) {
         _currentScreen.value = screen
+        savedStateHandle[CURRENT_SCREEN_KEY] = screen
     }
 }
