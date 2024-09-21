e8fc588257503ae33a4e67dc3c394bc74ec9bd0a
Francisco <franciscogarzonmoreno@gmail.com>
2024-09-20 23:09:29 -0500

    Obtencion de los datos de la tabla de grupos

--- app/src/main/java/com/example/oirapp/ui/screens/GruposScreen.kt
+++ app/src/main/java/com/example/oirapp/ui/screens/GruposScreen.kt
@@ -4,20 +4,43 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.unit.dp
 import com.example.oirapp.R
+import com.example.oirapp.data.network.SupabaseClient.supabaseClient
 import com.example.oirapp.ui.components.GroupCard
 import com.example.oirapp.ui.components.PrimaryButton
 import com.example.oirapp.ui.components.UserInfo
 import com.example.oirapp.ui.preview.DarkLightScreenPreviews
 import com.example.oirapp.ui.theme.MyApplicationTheme
+import io.github.jan.supabase.postgrest.postgrest
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.oirapp.data.network.Group
+import com.example.oirapp.ui.viewmodel.GruposViewModel
+import io.github.jan.supabase.postgrest.from
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 
 @Composable
 fun GruposScreen(
@@ -28,10 +51,18 @@ fun GruposScreen(
      */
     userImageUrl: String,
     modifier: Modifier = Modifier,
+    gruposViewModel: GruposViewModel = viewModel()
 ) {
-
+    var groupName by remember { mutableStateOf("") }
+    var grupos by remember { mutableStateOf<List<Group>>(listOf()) }
+    LaunchedEffect(Unit) {
+        withContext(Dispatchers.IO) {
+            grupos = supabaseClient.from("grupos")
+                .select().decodeList<Group>()
+        }
+    }
     println("userImageUrl: $userImageUrl")
-
+    println("grupos: $grupos")
     Surface(
         color = MaterialTheme.colorScheme.background,
         modifier = modifier.fillMaxSize(),
@@ -61,34 +92,33 @@ fun GruposScreen(
             /*
              * TODO: El estudiante puede unirse a un grupo con un código y este será mostrado en una lista
              */
+            TextField(
+                value = groupName,
+                onValueChange = { groupName = it },
+                label = { Text("Nombre del Grupo") },
+                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
+            )
 
-            Column(
+            LazyColumn(
                 verticalArrangement = Arrangement.spacedBy(24.dp),
                 modifier = Modifier.padding(top = 24.dp),
             ) {
-                GroupCard(
-                    groupName = "Cálculo Diferencial",
-                    groupCode = "3456",
-                    role = userRole,
-                )
-
-                GroupCard(
-                    groupName = "Emprendimiento I",
-                    groupCode = "3456",
-                    role = userRole,
-                )
-
-                GroupCard(
-                    groupName = "Álgebra Lineal",
-                    groupCode = "3456",
-                    role = userRole,
-                )
+                items(grupos) { group ->
+                    GroupCard(
+                        groupName = group.name,
+                        groupCode = group.code,
+                        role = userRole,
+                        onClick = {},
+                    )
+                }
             }
 
             Spacer(modifier = Modifier.weight(1f))
 
             PrimaryButton(
-                onClick = { },
+                onClick = {
+                    gruposViewModel.createGroup(groupName)
+                },
                 textId = if (userRole == "Estudiante") R.string.unirse_grupo else R.string.crear_grupo,
                 modifier = Modifier.padding(bottom = 20.dp),
             )
---
+++ app/src/main/java/com/example/oirapp/ui/viewmodel/GruposViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.oirapp.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.oirapp.data.network.SupabaseClient.supabaseClient
+import io.github.jan.supabase.postgrest.postgrest
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+
+class GruposViewModel : ViewModel() {
+    fun createGroup(groupName: String) {
+        val accessCode = generateAccessCode()
+        viewModelScope.launch {
+            createGroupSuspend(groupName, accessCode)
+        }
+    }
+
+    private suspend fun createGroupSuspend(groupName: String, accessCode: String) {
+        val table = supabaseClient.postgrest["grupos"]
+        table.insert(buildJsonObject {
+            put("nombre_grupo", groupName)
+            put("codigo_acceso", accessCode)
+            put("id_docente", "9499763e-b013-4e76-a714-52d8ba052886")
+        })
+    }
+
+    private fun generateAccessCode(): String {
+        val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return (1..6)
+            .map { chars.random() }
+            .joinToString("")
+    }
+
+}
