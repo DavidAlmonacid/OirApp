fd03925ec6452641733b1292c3a2d16900cb698a
Francisco <franciscogarzonmoreno@gmail.com>
2024-09-22 21:29:01 -0500

    Refactor MainApplication to remove unnecessary code

--- app/src/main/java/com/example/oirapp/MainApplication.kt
+++ app/src/main/java/com/example/oirapp/MainApplication.kt
@@ -154,6 +154,7 @@ fun MainApp(
                         }
                         navigationViewModel.updateCurrentScreen(MainApplication.Grupos)
                     }
+
                 }
             }
 
