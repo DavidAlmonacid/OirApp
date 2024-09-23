cd6fec95a112c98b55329622f97af95313f3aec1
David Almonacid <david.almonacidf@gmail.com>
2024-09-23 16:58:42 -0500

    Refactor MainActivity to check the current session

--- app/src/main/java/com/example/oirapp/MainActivity.kt
+++ app/src/main/java/com/example/oirapp/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.oirapp.data.network.SupabaseClient.supabaseClient
 import com.example.oirapp.ui.theme.MyApplicationTheme
+import io.github.jan.supabase.gotrue.auth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,21 +22,16 @@ class MainActivity : ComponentActivity() {
     override fun onStart() {
         super.onStart()
 
-        println("onStart: Starting App")
-
         /*
          * TODO: Verificar si el usuario ya ha iniciado sesión,
-         *  si es así, redirigir a la pantalla de Grupos dependiendo del rol del usuario,
-         *  si es 'Estudiante' redirigir a la pantalla de Grupos Estudiante,
-         *  si es 'Docente' redirigir a la pantalla de Grupos Docente.
+         *  si es así, redirigir a la pantalla de Grupos.
          */
 
-//        val currentUser = auth.currentUser
-//        if (currentUser != null) {
-//            val intent = Intent(this, InformacionAdicionalActivity::class.java)
-//            intent.apply { putExtra("USER_EMAIL", currentUser.email) }
-//            startActivity(intent)
-//            finish()
-//        }
+        val session = supabaseClient.auth.currentSessionOrNull()
+        println("MainActivity.onStart: Session: $session")
+
+        //if (session != null) {
+            // code
+        //}
     }
 }
