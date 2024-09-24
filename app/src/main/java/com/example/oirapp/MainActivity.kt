303b61a3e2493419f477cb7726c43dc6f9d91901
David Almonacid <david.almonacidf@gmail.com>
2024-09-24 16:00:27 -0500

    Remove some unused code

--- app/src/main/java/com/example/oirapp/MainActivity.kt
+++ app/src/main/java/com/example/oirapp/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.oirapp.data.network.SupabaseClient.supabaseClient
 import com.example.oirapp.ui.theme.MyApplicationTheme
-import io.github.jan.supabase.gotrue.auth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,21 +15,5 @@ class MainActivity : ComponentActivity() {
                 MainApp()
             }
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        /*
-         * TODO: Verificar si el usuario ya ha iniciado sesión,
-         *  si es así, redirigir a la pantalla de Grupos.
-         */
-
-        val session = supabaseClient.auth.currentSessionOrNull()
-        println("MainActivity.onStart: Session: $session")
-
-        //if (session != null) {
-            // code
-        //}
     }
 }
