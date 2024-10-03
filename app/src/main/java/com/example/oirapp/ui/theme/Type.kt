3a98638041967400f92cd8983dfbc7cc62f7b6de
David Almonacid <david.almonacidf@gmail.com>
2024-10-03 14:36:19 -0500

    Add custom Typography

--- app/src/main/java/com/example/oirapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/oirapp/ui/theme/Type.kt
@@ -1,49 +1,68 @@
 package com.example.oirapp.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.googlefonts.Font
-import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.oirapp.R
 
-val provider = GoogleFont.Provider(
-    providerAuthority = "com.google.android.gms.fonts",
-    providerPackage = "com.google.android.gms",
-    certificates = R.array.com_google_android_gms_fonts_certs
-)
-
 val bodyFontFamily = FontFamily(
-    Font(
-        googleFont = GoogleFont("Open Sans"),
-        fontProvider = provider,
-    )
+    Font(R.font.mukta_regular, FontWeight.Normal),
+    Font(R.font.mukta_medium, FontWeight.Medium),
+    Font(R.font.mukta_semibold, FontWeight.SemiBold),
+    Font(R.font.mukta_bold, FontWeight.Bold),
 )
 
-val displayFontFamily = FontFamily(
-    Font(
-        googleFont = GoogleFont("Open Sans"),
-        fontProvider = provider,
-    )
+val bodyFontFamilyMono = FontFamily(
+    Font(R.font.ubuntu_sans_mono_regular, FontWeight.Normal),
 )
-
-// Default Material 3 typography values
-val baseline = Typography()
 
 val Typography = Typography(
-    displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
-    displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
-    displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
-    headlineLarge = baseline.headlineLarge.copy(fontFamily = displayFontFamily),
-    headlineMedium = baseline.headlineMedium.copy(fontFamily = displayFontFamily),
-    headlineSmall = baseline.headlineSmall.copy(fontFamily = displayFontFamily),
-    titleLarge = baseline.titleLarge.copy(fontFamily = displayFontFamily),
-    titleMedium = baseline.titleMedium.copy(fontFamily = displayFontFamily),
-    titleSmall = baseline.titleSmall.copy(fontFamily = displayFontFamily),
+    displayMedium = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp,
+        fontFamily = bodyFontFamily,
+    ),
 
-    bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
-    bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
-    bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
-    labelLarge = baseline.labelLarge.copy(fontFamily = bodyFontFamily),
-    labelMedium = baseline.labelMedium.copy(fontFamily = bodyFontFamily),
-    labelSmall = baseline.labelSmall.copy(fontFamily = bodyFontFamily),
+    titleLarge = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp,
+        fontFamily = bodyFontFamily,
+    ),
+    titleMedium = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp,
+        fontFamily = bodyFontFamily,
+    ),
+
+    bodyLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+        fontFamily = bodyFontFamily,
+    ),
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp,
+        fontFamily = bodyFontFamily,
+    ),
+
+    labelMedium = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp,
+        fontFamily = bodyFontFamily,
+    )
 )
