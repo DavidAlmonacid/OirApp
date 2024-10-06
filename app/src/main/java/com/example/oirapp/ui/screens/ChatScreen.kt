ed363a6cf249e0234539a6eea4a3042114117480
David Almonacid <david.almonacidf@gmail.com>
2024-10-06 17:59:04 -0500

    Add preview

--- app/src/main/java/com/example/oirapp/ui/screens/ChatScreen.kt
+++ app/src/main/java/com/example/oirapp/ui/screens/ChatScreen.kt
@@ -1,9 +1,26 @@
 package com.example.oirapp.ui.screens
 
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.oirapp.ui.theme.MyApplicationTheme
 
 @Composable
 fun ChatScreen(modifier: Modifier = Modifier) {
     // TODO('Implement ChatScreen')
+    Surface(
+        onClick = { /*TODO*/ },
+        modifier = modifier,
+    ) {
+        // ChatScreenContent()
+    }
+}
+
+@Preview
+@Composable
+private fun ChatScreenPreview() {
+    MyApplicationTheme {
+        ChatScreen()
+    }
 }
