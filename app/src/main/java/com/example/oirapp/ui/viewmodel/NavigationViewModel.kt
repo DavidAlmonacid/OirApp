26ef7f056030c6894b90f6be6e2bfee26e529ecb
David Almonacid <david.almonacidf@gmail.com>
2024-10-07 18:10:39 -0500

    Add title management to NavigationViewModel

--- app/src/main/java/com/example/oirapp/ui/viewmodel/NavigationViewModel.kt
+++ app/src/main/java/com/example/oirapp/ui/viewmodel/NavigationViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.oirapp.ui.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.oirapp.MainApplication
@@ -13,8 +14,15 @@ class NavigationViewModel(private val savedStateHandle: SavedStateHandle) : View
     private val _currentScreen = savedStateHandle.getLiveData(CURRENT_SCREEN_KEY, MainApplication.Bienvenida)
     val currentScreen: LiveData<MainApplication> = _currentScreen
 
+    private val _title = MutableLiveData<String>()
+    val title: LiveData<String> get() = _title
+
     fun updateCurrentScreen(screen: MainApplication) {
         _currentScreen.value = screen
         savedStateHandle[CURRENT_SCREEN_KEY] = screen
+    }
+
+    fun updateTitle(newTitle: String) {
+        _title.value = newTitle
     }
 }
