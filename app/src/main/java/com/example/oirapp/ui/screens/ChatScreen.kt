16ec235a7cdc20c229c41dc640c1429f96dd97da
David Almonacid <david.almonacidf@gmail.com>
2024-10-09 17:32:47 -0500

    Improve chat screen

--- app/src/main/java/com/example/oirapp/ui/screens/ChatScreen.kt
+++ app/src/main/java/com/example/oirapp/ui/screens/ChatScreen.kt
@@ -1,12 +1,23 @@
 package com.example.oirapp.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.oirapp.R
 import com.example.oirapp.ui.theme.MyApplicationTheme
 
 @Composable
@@ -15,7 +26,31 @@ fun ChatScreen(modifier: Modifier = Modifier) {
         color = MaterialTheme.colorScheme.background,
         modifier = modifier.fillMaxSize(),
     ) {
-        Text(text = "Chat Screen")
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+        ) {
+            LazyColumn(
+                contentPadding = PaddingValues(vertical = 16.dp),
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+            ) {
+                items(36) {
+                    Text("Item ${it.inc()}")
+                }
+            }
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = { Text(stringResource(R.string.message)) },
+                )
+            }
+        }
     }
 }
 
