a86ba5acd59f014d43c7dc14a8171387c8550d86
David Almonacid <david.almonacidf@gmail.com>
2024-10-11 13:59:46 -0500

    Remove MessageUiState data class as it is no longer needed

--- app/src/main/java/com/example/oirapp/ui/state/MessageUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.oirapp.ui.state
-
-data class MessageUiState(
-    val id: String = "",
-    val senderId: String = "",
-    val message: String = "",
-    val timestamp: Long = 0L,
-    val groupId: String = "",
-)
