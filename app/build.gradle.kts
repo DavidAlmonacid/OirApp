45c49b878572bfdab9f01148b3a05e052ca1ca9c
David Almonacid <david.almonacidf@gmail.com>
2024-10-28 16:49:27 -0500

    Delete lines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,8 +70,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.navigation.compose)
-
-    // Compose UI libraries
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
