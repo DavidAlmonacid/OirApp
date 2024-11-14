5a945df381b6b874fd0f4830f665cd0009506afb
David Almonacid <david.almonacidf@gmail.com>
2024-11-14 09:38:23 -0500

    Remove unused imports

--- app/src/main/java/com/example/oirapp/ui/viewmodel/ChatViewModel.kt
+++ app/src/main/java/com/example/oirapp/ui/viewmodel/ChatViewModel.kt
@@ -27,11 +27,7 @@ import io.github.jan.supabase.storage.storage
 import io.github.jan.supabase.storage.upload
 import io.ktor.client.call.body
 import io.ktor.client.request.get
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
-import io.ktor.http.contentType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
